82b462538a5f7cbe91382aa209fb8d2ebe3d468b
Jeremy Bassi <bassi.jeremy@gmail.com>
2015-06-14 13:02:05 -0700

    Update login view to follow new logout flow.

--- couplr-ios/LoginViewController.swift
+++ couplr-ios/LoginViewController.swift
@@ -12,91 +12,20 @@ import CoreData
 class LoginViewController: UIViewController {
 
     let loginView: FBLoginView = FBLoginView()
-    let continueButton: UIButton = UIButton()
     let pageControl: UIPageControl = UIPageControl()
-    let attributionLabel: UILabel = UILabel()
     var viewControllerArray = Array<UIViewController>()
     var pageViewController: UIPageViewController!
 
     var viewDidAppear: Bool = false
     var viewIsVisible: Bool = false
+    var tutorialLoaded: Bool = false
 
     let socialGraphController = SocialGraphController.sharedInstance
     let matchGraphController = MatchGraphController.sharedInstance
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        loginView.hidden = true
-        continueButton.hidden = true
-        pageControl.hidden = true
-        
-        let viewWidth: CGFloat = view.bounds.width
-        let viewHeight: CGFloat = view.bounds.height
-        
-        loginView.delegate = self
-        loginView.readPermissions = ["user_friends", "user_status", "user_posts", "user_photos"]
-        let loginViewX: CGFloat = (view.bounds.width / 2) - (loginView.bounds.width / 2)
-        let loginViewY: CGFloat = view.bounds.height - 40 - (loginView.bounds.height / 2)
-        loginView.frame.origin = CGPointMake(loginViewX, loginViewY)
-        
-        let continueButtonY: CGFloat = (view.bounds.height / 2) + (loginView.frame.height / 2)
-        continueButton.frame = CGRectMake(0, continueButtonY, view.frame.size.width, 50)
-        continueButton.titleLabel?.adjustsFontSizeToFitWidth = true
-        continueButton.titleLabel?.textAlignment = .Center
-        continueButton.setTitleColor(view.tintColor, forState: .Normal)
-        continueButton.addTarget(self, action: "continueButtonPressed:", forControlEvents: .TouchUpInside)
-        
-        let matchTutorialPage = TutorialPageViewController()
-        matchTutorialPage.pageIndex = 0
-        matchTutorialPage.screenFileName = "tutorial-screen-match"
-        matchTutorialPage.descriptionText = kTutorialMatchDescription
-        let profileTutorialPage = TutorialPageViewController()
-        profileTutorialPage.pageIndex = 1
-        profileTutorialPage.screenFileName = "tutorial-screen-profile"
-        profileTutorialPage.descriptionText = kTutorialProfileDescription
-        let newsfeedTutorialPage = TutorialPageViewController()
-        newsfeedTutorialPage.pageIndex = 2
-        newsfeedTutorialPage.screenFileName = "tutorial-screen-newsfeed"
-        newsfeedTutorialPage.descriptionText = kTutorialNewsfeedDescription
-        
-        viewControllerArray.append(matchTutorialPage)
-        viewControllerArray.append(profileTutorialPage)
-        viewControllerArray.append(newsfeedTutorialPage)
-        
-        let pageControlX: CGFloat = (view.bounds.width / 2) - (pageControl.bounds.width / 2)
-        let pageControlY: CGFloat = loginViewY - 20
-        pageControl.frame.origin = CGPointMake(pageControlX, pageControlY)
-        pageControl.numberOfPages = viewControllerArray.count
-        pageControl.currentPage = 0
-        pageControl.pageIndicatorTintColor = UIColor.grayColor()
-        pageControl.currentPageIndicatorTintColor = UIColor.blackColor()
-        
-        pageViewController = UIPageViewController(transitionStyle: .Scroll, navigationOrientation: .Horizontal, options: nil)
-        pageViewController.dataSource = self
-        pageViewController.delegate = self
-        pageViewController.setViewControllers([viewControllerArray[0]], direction: .Forward, animated: false, completion: nil)
-        pageViewController.view.frame = CGRectMake(0, kStatusBarHeight+20, view.bounds.width, view.bounds.height-kStatusBarHeight-pageControl.frame.size.height-loginView.frame.size.height-70)
-        pageViewController.view.hidden = true
-        
-        attributionLabel.text = "Icons made by Freepik from www.flaticon.com. Flaticon is licensed by Creative Commons 3.0"
-        attributionLabel.lineBreakMode = NSLineBreakMode.ByWordWrapping
-        attributionLabel.numberOfLines = 0
-        attributionLabel.textAlignment = NSTextAlignment.Center
-        attributionLabel.font = UIFont(name: "HelveticaNeue-Light", size: 10)
-        attributionLabel.frame = CGRectMake(0, viewHeight - 36, viewWidth, 36).withMargin(horizontal: 10, vertical: 3)
-        attributionLabel.hidden = true
-        
-        view.backgroundColor = UIColor.whiteColor()
-        addChildViewController(pageViewController)
-        view.addSubview(pageViewController.view)
-        view.addSubview(pageControl)
-        view.addSubview(loginView)
-        view.addSubview(continueButton)
-        view.addSubview(attributionLabel)
-        
-        view.bringSubviewToFront(pageControl)
-        pageViewController.didMoveToParentViewController(self)
+        view.backgroundColor = kCouplrRedColor
     }
 
     override func viewDidAppear(animated: Bool) {
@@ -110,8 +39,10 @@ class LoginViewController: UIViewController {
             var activeSession: FBSession = FBSession.activeSession()
             if(settings.shouldSkipLogin || activeSession.isOpen) {
                 loadAppViewsAndPresentNavigationController(false)
+                loadTutorialAndPresentViews()
             } else {
                 viewIsVisible = true
+                loadTutorialAndPresentViews()
             }
             viewDidAppear = true
         }
@@ -122,6 +53,62 @@ class LoginViewController: UIViewController {
 
         CouplrSettingsManager.sharedInstance.shouldSkipLogin = true
         viewIsVisible = false
+    }
+    
+    func loadTutorialAndPresentViews() {
+        if !tutorialLoaded {
+            view.backgroundColor = UIColor.whiteColor()
+            
+            let viewWidth: CGFloat = view.bounds.width
+            let viewHeight: CGFloat = view.bounds.height
+            
+            loginView.delegate = self
+            loginView.readPermissions = ["user_friends", "user_status", "user_posts", "user_photos"]
+            let loginViewX: CGFloat = (view.bounds.width / 2) - (loginView.bounds.width / 2)
+            let loginViewY: CGFloat = view.bounds.height - 40 - (loginView.bounds.height / 2)
+            loginView.frame.origin = CGPointMake(loginViewX, loginViewY)
+            
+            let matchTutorialPage = TutorialPageViewController()
+            matchTutorialPage.pageIndex = 0
+            matchTutorialPage.screenFileName = "tutorial-screen-match"
+            matchTutorialPage.descriptionText = kTutorialMatchDescription
+            let profileTutorialPage = TutorialPageViewController()
+            profileTutorialPage.pageIndex = 1
+            profileTutorialPage.screenFileName = "tutorial-screen-profile"
+            profileTutorialPage.descriptionText = kTutorialProfileDescription
+            let newsfeedTutorialPage = TutorialPageViewController()
+            newsfeedTutorialPage.pageIndex = 2
+            newsfeedTutorialPage.screenFileName = "tutorial-screen-newsfeed"
+            newsfeedTutorialPage.descriptionText = kTutorialNewsfeedDescription
+            
+            viewControllerArray.append(matchTutorialPage)
+            viewControllerArray.append(profileTutorialPage)
+            viewControllerArray.append(newsfeedTutorialPage)
+            
+            let pageControlX: CGFloat = (view.bounds.width / 2) - (pageControl.bounds.width / 2)
+            let pageControlY: CGFloat = loginViewY - 20
+            pageControl.frame.origin = CGPointMake(pageControlX, pageControlY)
+            pageControl.numberOfPages = viewControllerArray.count
+            pageControl.currentPage = 0
+            pageControl.pageIndicatorTintColor = UIColor.grayColor()
+            pageControl.currentPageIndicatorTintColor = UIColor.blackColor()
+            
+            pageViewController = UIPageViewController(transitionStyle: .Scroll, navigationOrientation: .Horizontal, options: nil)
+            pageViewController.dataSource = self
+            pageViewController.delegate = self
+            pageViewController.setViewControllers([viewControllerArray[0]], direction: .Forward, animated: false, completion: nil)
+            pageViewController.view.frame = CGRectMake(0, kStatusBarHeight+20, view.bounds.width, view.bounds.height-kStatusBarHeight-pageControl.frame.size.height-loginView.frame.size.height-70)
+            
+            view.backgroundColor = UIColor.whiteColor()
+            addChildViewController(pageViewController)
+            view.addSubview(pageViewController.view)
+            view.addSubview(pageControl)
+            view.addSubview(loginView)
+            
+            view.bringSubviewToFront(pageControl)
+            pageViewController.didMoveToParentViewController(self)
+            tutorialLoaded = true
+        }
     }
     
     func loadAppViewsAndPresentNavigationController(animated: Bool) {
@@ -151,51 +138,6 @@ class LoginViewController: UIViewController {
             matchGraphController.reset()
         }
     }
-    
-    func continueButtonPressed(sender: UIButton) {
-        loadAppViewsAndPresentNavigationController(true)
-    }
-    
-    func hideTutorial() {
-        loginView.hidden = false
-        pageViewController.view!.hidden = true
-        pageControl.hidden = true
-        continueButton.hidden = false
-        attributionLabel.hidden = false
-        
-        pageViewController.removeFromParentViewController()
-        pageViewController.view.removeFromSuperview()
-        pageControl.removeFromSuperview()
-        let loginViewY: CGFloat = (view.bounds.height / 2) - (loginView.bounds.height / 2)
-        loginView.frame.origin.y = loginViewY
-    }
-    
-    func showTutorial() {
-        addChildViewController(pageViewController)
-        view.addSubview(pageViewController.view)
-        view.addSubview(pageControl)
-        
-        loginView.hidden = false
-        continueButton.hidden = true
-        pageViewController.view!.hidden = false
-        pageControl.hidden = false
-        attributionLabel.hidden = true
-        
-        let loginViewY: CGFloat = view.bounds.height - 40 - (loginView.bounds.height / 2)
-        loginView.frame.origin.y = loginViewY
-        
-        view.bringSubviewToFront(pageControl)
-        
-        pageViewController.view.alpha = 0.0
-        pageControl.alpha = 0.0
-        loginView.alpha = 0.0
-        
-        UIView.animateWithDuration(0.25, animations: {
-            self.pageViewController.view.alpha = 1.0
-            self.pageControl.alpha = 1.0
-            self.loginView.alpha = 1.0
-        })
-    }
 
 }
 
@@ -210,13 +152,7 @@ extension LoginViewController: FBLoginViewDelegate {
     }
     
     func loginViewShowingLoggedOutUser(loginView: FBLoginView!) {
-        showTutorial()
-    }
-    
-    func loginViewFetchedUserInfo(loginView : FBLoginView!, user: FBGraphUser) {
-        var buttonTitle: String = "Continue As \(user.name)..."
-        continueButton.setTitle(buttonTitle, forState: UIControlState.Normal)
-        hideTutorial()
+        loadTutorialAndPresentViews()
     }
     
     func loginView(loginView : FBLoginView!, handleError: NSError) {
